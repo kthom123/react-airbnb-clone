9e91b6b026bb3a7ad67307fc2d861a81146d2ef9
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-22 17:22:31 +0900

    props added

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,18 @@
 import React from "react"
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props)
   return (
     <div className="card">
-      <img src="./swimmer.png" alt="athlete" className="card--image" />
+      <img src={`./${props.img}`} alt="athlete" className="card--image" />
       <div className="card--stats">
         <img src="./star.png" alt="star" className="card--star" />
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
       </div>
-        <p>Life Lessons with Katie Zaferes</p>
-        <p><span className="bold">From $136</span> / person</p>
+        <p>{props.title}</p>
+        <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
   )
 }
